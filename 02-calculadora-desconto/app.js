7aaf43375e72b016287399ef3da244c7275ad5ab
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@CFP127ALUNO.LOCAL>
2026-03-17 13:26:04 -0300

    feat: finalizar calculadora-desconto

--- 02-calculadora-desconto/app.js
+++ 02-calculadora-desconto/app.js
@@ -12,16 +12,16 @@ function calcularDesconto() {
 
     const decimal = percentual.value / 100
 
+    removerClasses()
     if (percentual.value <= 5)
         resultado.classList.add('ate')
-    else if (percentual.value > 5 && percentual.value <= 10)
+    else if (percentual.value > 5 && percentual.value < 10)
         resultado.classList.add('entre')
     else
         resultado.classList.add('acima')
 
-    let valorFinal = preco.value - (preco.value * decimal)
-    let valorEconomizado = preco.value * decimal
+    let valorFinal = (preco.value - (preco.value * decimal)).toFixed(2)
+    let valorEconomizado = (preco.value * decimal).toFixed(2)
 
     resultado.textContent = `Valor economizado: ${valorEconomizado} Valor final: ${valorFinal}`
-
 }
